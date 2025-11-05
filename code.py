91e7eeeb8a07273f98b66f76440077bf6e340de4
Sandip <sr39@princeton.edu>
2025-11-05 11:15:47 -0800

    added a new variable to code.py

--- code.py
+++ code.py
@@ -4,8 +4,10 @@ import numpy as np
 ## define variables
 x=5
 y=x+2
+z=x*y
 
 ## diagnostics
-print(f"x={x}; y={y}")
+print(f"x={x};y={y};z={z}")
 print(f"x+y={x+y}")
 print(f"x-y={x-y}")
+print(f"x*y*z={x*y*z}")
